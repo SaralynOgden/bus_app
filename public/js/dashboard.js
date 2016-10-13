d7fb67ea20029d6cbfb328ebde1a7c74d601e654
SaralynOgden <Saralyn.Ogden@gmail.com>
2016-10-13 15:43:18 -0700

    Got rid of dashboard.js merge conflict

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,6 +1,7 @@
 (function() {
   'use strict';
-<<<<<<< HEAD
+
+  $('select').material_select();
 
   // $('.parallax').parallax(); Why is this here?
 
@@ -61,36 +62,4 @@
     .fail(() => {
       window.location.href = '/signup.html';
     });
-=======
-  $('select').material_select();
-  // $('.parallax').parallax(); ???
-
-  // $.getJSON('/user_buses')
-  //   .done((user_buses) => {
-  //     const busesInfo = $('#user_buses');
-  //
-  //     for (const busInfo of busesInfo) {
-  //       const $anchor = $('<a>')
-  //         .attr({
-  //           href: `/book.html?
-  //                   bus_number=${busInfo.busNumber}?
-  //                   stop_number=${busInfo.stopNumber}?
-  //                   start_time=${busInfo.startTime}?
-  //                   end_time=${busInfo.endTime}`,
-  //           'data-delay': '50'
-  //         })
-  //
-  //       const $row = $('<tr>').addClass('row');
-  //       const $busNumberCell = $(`<td>${busInfo.busNumber}</td>`);
-  //       const $stopNumberCell = $(`<td>${busInfo.stopNumber}</td>`);
-  //       const $startTimeCell = $(`<td>${busInfo.startTime}</td>`);
-  //       const $endTimeCell = $(`<td>${busInfo.endTime}</td>`);
-  //       const $deleteTimeCell = $(`<td>X</td>`)
-  //                                 .addClass('deleteCell');
-  //     }
-  //   })
-  //   .fail(() => {
-  //     window.location.href = '/signup.html';
-  //   });
->>>>>>> a2f76c8fda5e4af2a2ac73cfe547837b86e77959
 })();
