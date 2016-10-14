bd0a74e1c73cff6296f73ab8ecaaceeb24554a8b
SaralynOgden <Saralyn.Ogden@gmail.com>
2016-10-13 17:21:54 -0700

    first draft of POST /user_buses

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -25,40 +25,40 @@
     }
   };
 
+  const createRow = function(id, busNumber, stopNumber, startTime, endTime) {
+    const urlBase = `/post.html?
+                      bus_number=${busNumber}&
+                      stop_number=${stopNumber}&
+                      start_time=${startTime}&
+                      end_time=${endTime}`;
+          const $row = $(`<tr id="${busNumber + stopNumber +
+                                    startTime + endTime}">
+                            <td>${busNumber}</td>
+                            <td>${stopNumber}</td>
+                            <td>${startTime}</td>
+                            <td>${endTime}</td>
+                            <td>
+                              <span id="${"delete_" + busNumber +
+                                          stopNumber + startTime + endTime}">
+                                X
+                              </span>
+                            </td>
+                          </tr>`);
+          $(`#${busNumber + stopNumber + startTime + endTime}`).click(
+            (event) => {
+            event.preventDefault();
+            window.location.href = url;
+          });
+          $(`#${"delete_" + busNumber + stopNumber + startTime + endTime}`)
+              .click(deleteUserBus(`/delete/:id`));
+  };
+
   // $.getJSON('/user_buses')
   //   .done((userBuses) => {
   //
   //     for (const userBus of userBuses) {
-  //       const id = userBus.id;
-  //       const busNumber = userBus.busNumber;
-  //       const stopNumber = userBus.stopNumber;
-  //       const startTime = userBus.startTime;
-  //       const endTime = userBus.endTime;
-  //       const urlBase = `/post.html?
-  //                   bus_number=${busNumber}&
-  //                   stop_number=${stopNumber}&
-  //                   start_time=${startTime}&
-  //                   end_time=${endTime}`;
-  //       const $row = $(`<tr id="${busNumber + stopNumber +
-  //                                 startTime + endTime}">
-  //                         <td>${busNumber}</td>
-  //                         <td>${stopNumber}</td>
-  //                         <td>${startTime}</td>
-  //                         <td>${endTime}</td>
-  //                         <td>
-  //                           <span id="${"delete_" + busNumber +
-  //                                       stopNumber + startTime + endTime}">
-  //                             X
-  //                           </span>
-  //                         </td>
-  //                       </tr>`);
-  //       $(`#${busNumber + stopNumber + startTime + endTime}`).click(
-  //         (event) => {
-  //         event.preventDefault();
-  //         window.location.href = url;
-  //       });
-  //       $(`#${"delete_" + busNumber + stopNumber + startTime + endTime}`)
-  //           .click(deleteUserBus(`/delete/:id`));
+  //       createRow(userBus.id, userBus.busNumber, userBus.stopNumber,
+  //                 userBus.startTime, userBus.endTime);
   //     }
   //   })
   //   .fail(() => {
@@ -66,26 +66,41 @@
   //   });
 
   const addRoute = function(_event) {
-    const busNumber = 
-    const stopNumber =
-    const startTime =
-    const endTime =
+    const busNumber = $('bus-stop').val();
+    const stopNumber = $('bus-number').val();
+    const startTime = $('start-time-drop-down')[0].value;
+    const endTime = $('end-time-drop-down')[0].value;
+
+    if (!busNumber || !busNumber.trim()) {
+      return Materialize.toast('Bus number name must not be blank', 3000);
+    }
+    if (!stopNumber || !stopNumber.trim()) {
+      return Materialize.toast('Stop number must not be blank', 3000);
+    }
+    if (!startTime || !startTime.trim()) {
+      return Materialize.toast('Start time must not be blank', 3000);
+    }
+    if (!endTime || !endTime.trim()) {
+      return Materialize.toast('End time must not be blank', 3000);
+    }
+
     const options = {
       contentType: 'application/json',
-      data: JSON.stringify({ email, password }),
+      data: JSON.stringify({ busNumber, stopNumber, startTime, endTime }),
       dataType: 'json',
       type: 'POST',
-      url: '/token'
+      url: '/user_buses'
     };
 
     $.ajax(options)
-      .done((deletedUserBus) => {
-        $(`${deletedUserBus.busNumber + deletedUserBus.stopNumber +
-             deletedUserBus.startTime + deletedUserBus.endTime}`).remove();
+      .done((postedUserBus) => {
+        createRow(postedUserBus.id, postedUserBus.busNumber,
+          postedUserBus.stopNumber, postedUserBus.startTime,
+          postedUserBus.endTime);
       })
       .fail(() => {
         Materialize.toast(
-          'Unable to delete route. Please try again.', 3000);
+          'Unable to create route. Please try again.', 3000);
       });
   };
 
