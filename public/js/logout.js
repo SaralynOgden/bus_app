c9ea22c0a000770360321c96a7d33715868b081d
Chase-Klingel <chaseklingel@Chases-MacBook-Pro.local>
2016-10-15 14:45:28 -0700

    logout functionality working

--- public/js/logout.js
+++ public/js/logout.js
@@ -27,13 +27,3 @@ $.getJSON('/token')
   .fail(($xhr) => {
     Materialize.toast($xhr.responseText, 3000);
   });
-
-// window.QUERY_PARAMETERS = {};
-//
-// if (window.location.search) {
-//   window.location.search.substr(1).split('&').forEach((paramStr) => {
-//     const param = paramStr.split('=');
-//
-//     window.QUERY_PARAMETERS[param[0]] = param[1];
-//   });
-// }
