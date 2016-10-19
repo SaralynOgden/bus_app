8f1dafe4685cb38be3631f349cd371d5e49c9742
SaralynOgden <Saralyn.Ogden@gmail.com>
2016-10-18 18:15:08 -0700

    preliminary draft of extracting data for plots in plot.js

--- public/js/plot.js
+++ public/js/plot.js
@@ -7,12 +7,49 @@
     window.location.href = '/';
   }
 
+  const getJSDateInPDT = function(sqlDate) {
+    const year = sqlDate.substring(0, 4),
+          month = sqlDate.substring(5, 7) - 1,
+          date = sqlDate.substring(8, 10),
+          hour = sqlDate.substring(11, 13),
+          minute = sqlDate.substring(14, 16)
+          dateInGMT = new Date(year, month, date, hour, minute);
 
+    return new Date(dateInGMT.getTime() - 7 * 60 * 60 * 1000)
+  };
 
-  $.getJSON(`/data/where?tripId=${tripId}&$stopNumber=${stopNumber}`)
+  const getScheduledTimeDictionary = function(processedTripData) {
+    const scheduledTimeDictionary = {};
+
+    for (let tripDatum of processedTripData) {
+      const scheduledTime = getJSDateInPDT(tripDatum.scheduledTime);
+      let dateArray;
+
+      if (scheduledTime in scheduledTimeDictionary) {
+        dateArray = scheduledTimeDictionary[scheduledTime];
+      } else {
+        dateArray = [[], [], [], [], []];
+      }
+
+      dateArray = insertActualTimeInDateArray(tripDatum.actualTime, dateArray);
+      scheduledTimeDictionary[scheduledTime] = dateArray;
+    }
+
+    return scheduledTimeDictionary;
+  };
+
+  const insertActualTimeInDateArray = function(actualTime, dateArray) {
+    const actualTimeJS = getJSDateInPDT(actualTime);
+
+    dateArray[acutalTime.getDay()].push(actualTime);
+
+    return dateArray;
+  };
+
+  $.getJSON(`/data/where?tripId=${tripId}&stopNumber=${stopNumber}`)
   .done((rows) => {
-
-
+    const processedTripData = decamelizeKeys(rows),
+          scheduledTimeDictionary = getScheduledTimeDictionary(processedTripData);
   })
   .fail(() => {
     Materialize.toast('Unable to retrieve data', 3000);
