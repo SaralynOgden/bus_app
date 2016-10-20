ad889adffbe96f32854590d83d5ecbf39d632763
Chase-Klingel <chaseklingel@Chases-MacBook-Pro.local>
2016-10-19 18:22:52 -0700

    added eslint file

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: [
+    'ryansobol/es6',
+    'ryansobol/mocha',
+    'ryansobol/node'
+  ],
+
+  rules: {
+    'brace-style': [2, '1tbs', { allowSingleLine: true }],
+  }
+};
