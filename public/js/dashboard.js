52d27bff6326e3900e19f89b72359ddca30804dd
Chase-Klingel <chaseklingel@Chases-MacBook-Pro.local>
2016-10-20 22:47:56 -0700

    testing delete

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -53,6 +53,7 @@
       $.ajax(options)
         .done((deletedTrip) => {
           $(`#trip_${id}`).remove();
+          window.location.href = 'dashboard.html';
         })
         .fail(() => {
           Materialize.toast(
@@ -99,12 +100,12 @@
 
     $('tbody').append($row);
 
-    $(`#delete_trip_${tripId}`).click(deleteTrip(tripId));
-
     $(`#trip_${tripId}`).click((event) => {
       event.preventDefault();
       window.location.href = url;
     });
+
+    $(`#delete_trip_${tripId}`).click(deleteTrip(tripId));
   };
 
   $.getJSON('/trips_users')
